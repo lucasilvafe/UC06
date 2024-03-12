9a05b591842266b2c4771ce9f2dab686185ccb2c
DevLucas <luksilvafe@gmail.com>
2024-03-12 17:19:58 -0300

    att05

---
+++ SQLQuery3.sql
@@ -0,0 +1,64 @@
+create database instagram
+use instagram
+create table instagram(
+	
+);
+-- Criar tabela usuario
+create table perfil_usuario(
+	id_usuario int primary key,
+	nome_usuario varchar(10),
+	telefone varchar(12),
+	email nvarchar(30),
+	senha nvarchar(20)
+);
+create table login_usuario(
+
+);
+create table pessoal(
+	
+);
+create table pesquisa(
+	
+);
+create table beta(
+	
+);
+create table criador_conteudo(
+	
+);
+create table comercial(
+	
+);
+create table pessoal(
+	
+);
+create table direct(
+	
+);
+create table audio(
+	
+);
+create table imagem(
+	
+);
+create table texto(
+	
+);
+create table chamada(
+	
+);
+create table video(
+	
+);
+create table postagem(
+	
+);
+create table feed(
+	
+);
+create table reel(
+	
+);
+create table story(
+	
+);
